70ddcea074ed1065982e9bd25d4ca0a776af9fbc
John Carlo Gamboa <hddnblde@gmail.com>
2021-06-05 13:14:25 +0800

    DialogueWindow: made several changes to support yield instruction from VN Controller; added skip dialogue function.

--- Assets/archipelaGO/Scripts/Visual Novel/UI/Windows/DialogueWindow.cs
+++ Assets/archipelaGO/Scripts/Visual Novel/UI/Windows/DialogueWindow.cs
@@ -1,18 +1,81 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace archipelaGO.VisualNovel.UI.Windows
 {
     public sealed class DialogueWindow : UIWindow
     {
+        #region Fields
         [SerializeField]
         private AnimatedText m_animatedText = null;
 
-        public void Show(string text)
+        [SerializeField]
+        private Button m_skipButton = null;
+
+        [SerializeField]
+        private bool m_showNextDialogue = false;
+        #endregion
+
+
+        #region MonoBehaviour Implementation
+        private void OnEnable() => AddListenerToSkipButton();
+        private void OnDisable() => RemoveListenerFromSkipButton();
+        #endregion
+
+
+        #region Skip Button Implementation
+        private void AddListenerToSkipButton()
+        {
+            if (m_skipButton != null)
+                m_skipButton.onClick.AddListener(OnSkipButtonPressed);
+        }
+
+        private void RemoveListenerFromSkipButton()
+        {
+            if (m_skipButton != null)
+                m_skipButton.onClick.RemoveListener(OnSkipButtonPressed);
+        }
+
+        private void OnSkipButtonPressed()
         {
             if (m_animatedText != null)
-                m_animatedText.ShowText(text, Color.white);
-
-            ShowCanvas(true);
+                m_animatedText.SkipTextScrolling();
         }
+        #endregion
+
+
+        #region Public Methods
+        public override void Hide()
+        {
+            m_animatedText.SkipTextScrolling();
+            m_showNextDialogue = false;
+            base.Hide();
+        }
+
+        public IEnumerator ShowDialogueLine(Sprite characterSprite, string characterName, string dialogueLine)
+        {
+            m_showNextDialogue = false;
+            Show();
+            SetCharacter(characterSprite, characterName);
+            yield return AnimateDialogueLine(dialogueLine);
+            yield return new WaitUntil(() => m_showNextDialogue);
+            Hide();
+        }
+        #endregion
+
+
+        #region Internal Methods
+        private void SetCharacter(Sprite sprite, string name)
+        {
+            
+        }
+
+        private IEnumerator AnimateDialogueLine(string dialogueLine)
+        {
+            if (m_animatedText != null)
+                yield return m_animatedText.ShowText(dialogueLine, Color.white);
+        }
+        #endregion
     }
 }
