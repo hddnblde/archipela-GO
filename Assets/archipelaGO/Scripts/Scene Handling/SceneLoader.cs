90745d81f59b1ad3cec9d4bf2de36e6040354a2a
John Carlo Gamboa <hddnblde@gmail.com>
2021-06-26 16:00:44 +0800

    Moved SceneLoader script into the 'Scene Handling' directory.

--- Assets/archipelaGO/Scripts/Scene Handling/SceneLoader.cs
+++ Assets/archipelaGO/Scripts/Scene Handling/SceneLoader.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace archipelaGO
+namespace archipelaGO.SceneHandling
 {
     public class SceneLoader : MonoBehaviour
     {
