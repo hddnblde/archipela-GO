fb163b3d77524ae3a5dc7abd05cb8740fc3e711b
John Carlo Gamboa <hddnblde@gmail.com>
2021-06-26 09:43:07 +0800

    Initial implementation of WorldClickableObject.

---
+++ Assets/archipelaGO/Scripts/World Map/WorldClickableObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace archipelaGO.WorldMap
+{
+    public class WorldClickableObject : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+            
+        }
+    }
+}
