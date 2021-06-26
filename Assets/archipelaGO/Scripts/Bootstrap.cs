da2ddaeadaab4443ce7fd0a2bd67b62a4614bbc7
John Carlo Gamboa <hddnblde@gmail.com>
2021-06-26 15:54:52 +0800

    Implemented Bootstrap that will launch the game once Boot screen loads.

---
+++ Assets/archipelaGO/Scripts/Bootstrap.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace archipelaGO
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        [SerializeField]
+        private SceneLoader m_sceneLoader = null;
+
+        [SerializeField]
+        private string m_firstSceneToLoad = string.Empty;
+
+        private void Start()
+        {
+            if (m_sceneLoader != null)
+                m_sceneLoader.LoadScene(m_firstSceneToLoad);
+        }
+    }
+}
