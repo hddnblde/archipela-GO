a48dacc0244e23ff658fe99c5a5880e3db9db39f
John Carlo Gamboa <hddnblde@gmail.com>
2021-07-04 10:42:09 +0800

    Implemented QuizConfig.

---
+++ Assets/archipelaGO/Scripts/Quiz/QuizConfig.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace archipelaGO.Quiz
+{
+    [CreateAssetMenu(fileName = "Quiz", menuName = "archipelaGO/Quiz", order = 2)]
+    public class QuizConfig : ScriptableObject
+    {
+        #region Fields
+        [SerializeField]
+        private List<Question> m_questions = new List<Question>();
+        #endregion
+
+
+        #region Methods
+        public List<Question> GenerateRandomSetOfQuestions(int count)
+        {
+            List<Question> shuffledQuestions = new List<Question>();
+            shuffledQuestions.AddRange(m_questions);
+            shuffledQuestions = ShuffleList<Question>(shuffledQuestions);
+
+            if (shuffledQuestions.Count > count)
+            {
+                int difference = (shuffledQuestions.Count - count);
+                shuffledQuestions.RemoveRange(count, difference);
+            }
+
+            return shuffledQuestions;
+        }
+
+        private static List<T> ShuffleList<T>(List<T> list)
+        {
+            List<T> shuffledList = new List<T>();
+            shuffledList.AddRange(list);
+
+            for (int i = 0; i < shuffledList.Count; i++)
+            {
+                T targetElement = shuffledList[i];
+                int randomIndex = Random.Range(0, shuffledList.Count);
+                T swappedElement = shuffledList[randomIndex];
+
+                shuffledList[i] = swappedElement;
+                shuffledList[randomIndex] = targetElement;
+            }
+
+            return shuffledList;
+        }
+        #endregion
+
+
+        #region Data Structure
+        [System.Serializable]
+        public struct Question
+        {
+            #region Fields
+            [SerializeField]
+            private string m_stem;
+
+            [SerializeField]
+            private string m_correctAnswer;
+
+            [SerializeField]
+            private List<string> m_incorrectAnswers;
+            #endregion
+
+            public (string stem, List<string> choices, int correctAnswerIndex) GetProblem()
+            {
+                List<string> choices = new List<string>();
+                choices.Add(m_correctAnswer);
+                choices.AddRange(m_incorrectAnswers);
+                choices = ShuffleList<string>(choices);
+
+                int correctAnswerIndex = choices.IndexOf(m_correctAnswer);
+
+                return (m_stem, choices, correctAnswerIndex);
+            }
+        }
+        #endregion
+    }
+}
