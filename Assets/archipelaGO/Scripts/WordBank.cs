002da6c37b6c4b72debb6c3d9df881c5a3467b52
John Carlo Gamboa <hddnblde@gmail.com>
2021-07-24 15:33:35 +0800

    WordBank: added 'PartOfSpeech' enum.

--- Assets/archipelaGO/Scripts/WordBank.cs
+++ Assets/archipelaGO/Scripts/WordBank.cs
@@ -19,11 +19,25 @@ namespace archipelaGO
             [SerializeField]
             private string m_title;
 
+            [SerializeField]
+            private PartOfSpeech m_partOfSpeech;
+
             [SerializeField, TextArea]
             private string m_description;
 
             public string title => m_title;
+            public PartOfSpeech partOfSpeech => m_partOfSpeech;
             public string description => m_description;
+        }
+
+        [System.Serializable]
+        public enum PartOfSpeech
+        {
+            Noun = 0,
+            Pronoun = 1,
+            Adjective = 2,
+            Verb = 3,
+            Adverb = 4
         }
         #endregion
 
