80fa70071988c05a17f3a9d0ad732a1e55c8d704
John Carlo Gamboa <hddnblde@gmail.com>
2021-07-31 11:47:13 +0800

    CrosswordPuzzle: removed CrosswordDirection enum.

--- Assets/archipelaGO/Scripts/Puzzle/CrosswordPuzzle.cs
+++ Assets/archipelaGO/Scripts/Puzzle/CrosswordPuzzle.cs
@@ -2,7 +2,6 @@ using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using GridDirection = archipelaGO.Puzzle.CrosswordDirection;
 using GridWord = archipelaGO.Puzzle.WordPuzzle.GridWord;
 
 namespace archipelaGO.Puzzle
