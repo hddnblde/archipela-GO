754698e0015b976b1078f82527df0aaa6739c28d
John Carlo Gamboa <hddnblde@gmail.com>
2021-08-21 14:06:34 +0800

    Implemented NarrataiveDictionaryPostProcessorWindow.

---
+++ Assets/archipelaGO/Scripts/Visual Novel/Story System/Narratives/Editor/NarrativeDictionaryPostProcessorWindow.cs
@@ -0,0 +1,139 @@
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace archipelaGO.VisualNovel.StorySystem.Narratives
+{
+    public class NarrativeDictionaryPostProcessorWindow : EditorWindow
+    {
+        [SerializeField]
+        private WordBank m_wordBank = null;
+
+        [SerializeField]
+        private List<LinearNarrative> m_narrativeLines = new List<LinearNarrative>();
+
+        private SerializedObject m_serializedObject = null;
+        private SerializedProperty m_wordBankProperty = null,
+            m_narrativeLinesProperty = null;
+
+        [MenuItem("Utility/Narrative line Post-processor")]
+        private static void InitializeWindow()
+        {
+            NarrativeDictionaryPostProcessorWindow window = EditorWindow.GetWindow<NarrativeDictionaryPostProcessorWindow>();
+            window.Show();
+        }
+
+        private void Awake() => InitializeSerializedProperties();
+
+        private void OnGUI()
+        {
+            if (m_serializedObject == null)
+                InitializeSerializedProperties();
+
+            using (var scope = new EditorGUI.ChangeCheckScope())
+            {
+                DrawProperties();
+
+                if (scope.changed)
+                    m_serializedObject.ApplyModifiedProperties();
+            }
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Process"))
+            {
+                ProcessWords();
+                AssetDatabase.Refresh();
+            }
+        }
+
+        private void DrawProperties()
+        {
+            EditorGUILayout.PropertyField(m_wordBankProperty);
+            EditorGUILayout.PropertyField(m_narrativeLinesProperty);
+        }
+
+        private void InitializeSerializedProperties()
+        {
+            m_serializedObject = new SerializedObject(this);
+            m_wordBankProperty = m_serializedObject.FindProperty("m_wordBank");
+            m_narrativeLinesProperty = m_serializedObject.FindProperty("m_narrativeLines");
+        }
+
+        private void ProcessWords()
+        {
+            List<string> orderedWords = GetWordCollectionFromBank();
+
+            if (orderedWords == null)
+                return;
+            
+            for (int i = 0; i < m_narrativeLinesProperty.arraySize; i++)
+            {
+                SerializedProperty narrativeLine = m_narrativeLinesProperty.GetArrayElementAtIndex(i);
+                ProcessWordsOnNarrativeLine(narrativeLine, orderedWords);
+            }
+        }
+
+        private void ProcessWordsOnNarrativeLine(SerializedProperty narrativeLine, List<string> words)
+        {
+            SerializedObject narrativeLineObject = new SerializedObject(narrativeLine.objectReferenceValue);
+            SerializedProperty dialoguesProperty = narrativeLineObject.FindProperty("m_dialogues");
+
+            for (int i = 0; i < dialoguesProperty.arraySize; i++)
+            {
+                SerializedProperty dialogueProperty = dialoguesProperty.GetArrayElementAtIndex(i);
+                ProcessDialogueLine(dialogueProperty, words);
+            }
+
+            narrativeLineObject.ApplyModifiedPropertiesWithoutUndo();
+        }
+
+        private void ProcessDialogueLine(SerializedProperty dialogueLine, List<string> words)
+        {
+            SerializedProperty lines = dialogueLine.FindPropertyRelative("m_lines");
+
+            for (int i = 0; i < lines.arraySize; i++)
+            {
+                SerializedProperty line = lines.GetArrayElementAtIndex(i);
+                SerializedProperty text = line.FindPropertyRelative("m_text");
+                ProcessText(text, words);
+            }
+        }
+
+        private void ProcessText(SerializedProperty textProperty, List<string> words)
+        {
+            string textValue = textProperty.stringValue;
+            textValue = textValue.Replace("{", string.Empty).Replace("}", string.Empty);
+
+            foreach (string word in words)
+            {
+                string pattern = $@"{word}";
+                Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+                MatchCollection matches = regex.Matches(textValue);
+
+                foreach (Match match in matches)
+                    textValue = textValue.Replace(match.Value, $"{{{match.Value}}}");
+            }
+
+            textProperty.stringValue = textValue;
+        }
+
+        private List<string> GetWordCollectionFromBank()
+        {
+            if (m_wordBank == null)
+                return null;
+
+            List<string> collection = new List<string>();
+            
+            for (int i = 0; i < m_wordBank.wordCount; i++)
+            {
+                string word = m_wordBank.GetWord(i).title;
+                collection.Add(word);
+            }
+
+            return collection.OrderByDescending(s => s).ToList();
+        }
+    }
+}
