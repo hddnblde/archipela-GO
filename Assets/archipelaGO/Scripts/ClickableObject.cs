292ed8242a175a2f36f86274b10ff5847ca162d1
John Carlo Gamboa <hddnblde@gmail.com>
2021-08-21 16:39:01 +0800

    ClickableObject: added abstract function IsInteractable.

--- Assets/archipelaGO/Scripts/ClickableObject.cs
+++ Assets/archipelaGO/Scripts/ClickableObject.cs
@@ -5,9 +5,13 @@ namespace archipelaGO
 {
     public abstract class ClickableObject : MonoBehaviour, IPointerClickHandler
     {
-        public void OnPointerClick(PointerEventData pointerEventData) =>
-            OnObjectClicked();
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+            if (IsInteractable())
+                OnObjectClicked();
+        }
         
         protected abstract void OnObjectClicked();
+        protected abstract bool IsInteractable();
     }
 }
