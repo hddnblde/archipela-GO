cdbc65665bcffb369cab0944eb58b172e081df66
John Carlo Gamboa <hddnblde@gmail.com>
2021-09-04 11:46:09 +0800

    GmaeModuleLibrary: added fields and functions.

--- Assets/archipelaGO/Scripts/Game/GameModuleLibrary.cs
+++ Assets/archipelaGO/Scripts/Game/GameModuleLibrary.cs
@@ -6,7 +6,45 @@ namespace archipelaGO.Game
     [CreateAssetMenu(fileName = "Game Module Library", menuName = "archipelaGO/Game Module Library")]
     public class GameConfigLibrary : ScriptableObject
     {
+        #region Field
         [SerializeField]
-        private List<GameModuleConfig> m_modules = new List<GameModuleConfig>();
+        private List<ModuleNode> m_modules = new List<ModuleNode>();
+        #endregion
+
+
+        #region Data Structure
+        public class ModuleNode
+        {
+            #region Fields
+            [SerializeField]
+            private GameModuleConfig m_config = null;
+
+            [SerializeField]
+            private Vector2 m_position = Vector2.zero;
+            #endregion
+
+
+            #region Properties
+            public GameModuleConfig config => m_config;
+            public Vector2 position => m_position;
+            #endregion
+        }
+        #endregion
+
+
+        #region Property
+        public int moduleCount => m_modules.Count;
+        #endregion
+
+
+        #region Method
+        public ModuleNode GetNode(int index)
+        {
+            if (index < 0 || index >= moduleCount)
+                return null;
+
+            return m_modules[index];
+        }
+        #endregion
     }
 }
