2542b9afd720c93e7fe99944a197a9a4adb8c449
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-02 09:14:19 +0800

    GameModuleConfig: added 'hint' field.

--- Assets/archipelaGO/Scripts/Game/GameModuleConfig.cs
+++ Assets/archipelaGO/Scripts/Game/GameModuleConfig.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 namespace archipelaGO.Game
 {
-    public abstract class GameModuleConfig : ScriptableObject {}
+    public abstract class GameModuleConfig : ScriptableObject
+    {
+        [SerializeField]
+        private string m_hint = string.Empty;
+
+        [SerializeField]
+        private bool m_requireTapToContinue = false;
+
+        public string hint => m_hint;
+        public bool requireTapToContinue => m_requireTapToContinue;
+    }
 }
