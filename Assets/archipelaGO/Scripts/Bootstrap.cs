4b19e2f4ddaf34673e5276f4f599b168b599fe70
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-02 20:44:38 +0800

    Implemented splash screen.

--- Assets/archipelaGO/Scripts/Bootstrap.cs
+++ Assets/archipelaGO/Scripts/Bootstrap.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Video;
+using System.Collections;
 using archipelaGO.SceneHandling;
 
 namespace archipelaGO
@@ -6,8 +8,28 @@ namespace archipelaGO
     public class Bootstrap : MonoBehaviour
     {
         [SerializeField]
+        private float m_delayBeforeLoadingFirstScene = 5f;
+
+        [SerializeField]
+        private VideoPlayer m_splashScreenPlayer = null;
+
+        [SerializeField]
         private Scene m_firstSceneToLoad = Scene.WorldMap;
 
-        private void Start() => SceneLoader.LoadScene(m_firstSceneToLoad);
+        private void Start() => StartCoroutine(BootRoutine());
+
+        private IEnumerator BootRoutine()
+        {
+            if (m_splashScreenPlayer != null)
+            {
+                m_splashScreenPlayer.Prepare();
+                yield return new WaitUntil(() => m_splashScreenPlayer.isPrepared);
+                m_splashScreenPlayer.Play();
+                yield return new WaitUntil(() => !m_splashScreenPlayer.isPlaying);
+                yield return new WaitForSeconds(m_delayBeforeLoadingFirstScene);
+            }
+
+            SceneLoader.LoadScene(m_firstSceneToLoad);
+        }
     }
 }
