c0e144ff52dfe21b22f6d91ee05f4f9cf2a2e2d6
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-03 11:19:12 +0800

    DialogueCharacter: implemented stage blocking.

--- Assets/archipelaGO/Scripts/Visual Novel/Dialogue System/Elements/DialogueCharacter.cs
+++ Assets/archipelaGO/Scripts/Visual Novel/Dialogue System/Elements/DialogueCharacter.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 namespace archipelaGO.VisualNovel.DialogueSystem.Elements
 {
     [System.Serializable]
+    public enum DialogueCharacterBlocking
+    {
+        StageLeft = 0,
+        StageRight = 1
+    }
+
+    [System.Serializable]
     public class DialogueCharacter
     {
         #region Fields
+        [SerializeField]
+        private DialogueCharacterBlocking m_blocking;
+
         [SerializeField]
         private Sprite m_sprite;
 
@@ -15,6 +25,7 @@ namespace archipelaGO.VisualNovel.DialogueSystem.Elements
 
 
         #region Properties
+        public DialogueCharacterBlocking blocking => m_blocking;
         public Sprite sprite => m_sprite;
         public string characterName => m_name;
         #endregion
