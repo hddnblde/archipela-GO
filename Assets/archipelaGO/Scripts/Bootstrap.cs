aced3749d0cfe2c71f4f9ebc10fb1928ab707626
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-03 09:29:16 +0800

    Bootstrap: added debug flag to skip splash screen.

--- Assets/archipelaGO/Scripts/Bootstrap.cs
+++ Assets/archipelaGO/Scripts/Bootstrap.cs
@@ -7,6 +7,12 @@ namespace archipelaGO
 {
     public class Bootstrap : MonoBehaviour
     {
+        #region Fields
+        #if UNITY_EDITOR
+        [SerializeField]
+        private bool m_skipSplashScreen = false;
+        #endif
+
         [SerializeField]
         private float m_delayBeforeLoadingFirstScene = 5f;
 
@@ -15,11 +21,22 @@ namespace archipelaGO
 
         [SerializeField]
         private Scene m_firstSceneToLoad = Scene.WorldMap;
+        #endregion
 
+
+        #region MonoBehaviour Implementation
         private void Start() => StartCoroutine(BootRoutine());
+        #endregion
 
+
+        #region Internal Methods
         private IEnumerator BootRoutine()
         {
+            #if UNITY_EDITOR
+            if (m_skipSplashScreen)
+                goto LoadFirstScene;
+            #endif
+
             if (m_splashScreenPlayer != null)
             {
                 m_splashScreenPlayer.Prepare();
@@ -29,7 +46,9 @@ namespace archipelaGO
                 yield return new WaitForSeconds(m_delayBeforeLoadingFirstScene);
             }
 
+            LoadFirstScene:
             SceneLoader.LoadScene(m_firstSceneToLoad);
         }
+        #endregion
     }
 }
