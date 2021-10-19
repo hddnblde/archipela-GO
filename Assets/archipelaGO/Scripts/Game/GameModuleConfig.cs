9b6442a55c25ded6e2e07858d9091a3ecf90dbee
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-19 17:47:04 +0800

    GameModuleConfig: added 'end message' field.

--- Assets/archipelaGO/Scripts/Game/GameModuleConfig.cs
+++ Assets/archipelaGO/Scripts/Game/GameModuleConfig.cs
@@ -8,9 +8,13 @@ namespace archipelaGO.Game
         private string m_hint = string.Empty;
 
         [SerializeField]
+        private string m_endMessage = string.Empty;
+
+        [SerializeField]
         private bool m_requireTapToContinue = false;
 
         public string hint => m_hint;
+        public string endMessage => m_endMessage;
         public bool requireTapToContinue => m_requireTapToContinue;
     }
 }
