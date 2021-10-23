fa99256c273558b20781c6701a63c742afb16685
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-23 21:27:58 +0800

    DialogueCharacter: added 'scale' and 'pivotOffset' properties.

--- Assets/archipelaGO/Scripts/Visual Novel/Dialogue System/Elements/DialogueCharacter.cs
+++ Assets/archipelaGO/Scripts/Visual Novel/Dialogue System/Elements/DialogueCharacter.cs
@@ -14,16 +14,24 @@ namespace archipelaGO.VisualNovel.DialogueSystem.Elements
     {
         #region Fields
         [SerializeField]
-        private string m_name;
+        private string m_name = string.Empty;
 
         [SerializeField]
-        private Sprite m_sprite;
+        private Sprite m_sprite = null;
+
+        [SerializeField]
+        private Vector2 m_pivotOffset = Vector2.zero;
+
+        [SerializeField, Range(0.01f, 3f)]
+        private float m_scale = 1f;
         #endregion
 
 
         #region Properties
         public Sprite sprite => m_sprite;
         public string displayedName => m_name;
+        public Vector2 pivotOffset => m_pivotOffset;
+        public float scale => m_scale;
         #endregion
     }
 }
