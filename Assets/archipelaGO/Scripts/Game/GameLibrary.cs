f901b116aea1764875f699007b00fd0b3aefdc69
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-24 14:03:23 +0800

    GameLibrary: added 'requiredKeys' and 'unlockedKeys' properties to ModuleNode -- this determines whether a node is unlocked or not, and determine which nodes can unlock which.

--- Assets/archipelaGO/Scripts/Game/GameLibrary.cs
+++ Assets/archipelaGO/Scripts/Game/GameLibrary.cs
@@ -11,6 +11,9 @@ namespace archipelaGO.Game
         private Sprite m_worldBackground = null;
 
         [SerializeField]
+        private GameProgressCollection m_progressKeys = null;
+
+        [SerializeField]
         private List<Sprite> m_nodeSprites = new List<Sprite>();
 
         [SerializeField]
@@ -34,6 +37,12 @@ namespace archipelaGO.Game
 
             [SerializeField]
             private int m_spriteIndex = 0;
+
+            [SerializeField]
+            private List<GameModuleConfig> m_requiredKeys = new List<GameModuleConfig>();
+
+            [SerializeField]
+            private List<GameModuleConfig> m_unlockedKeys = new List<GameModuleConfig>();
             #endregion
 
 
@@ -42,6 +51,31 @@ namespace archipelaGO.Game
             public GameModuleConfig config => m_config;
             public Vector2 position => m_position;
             public int spriteIndex => m_spriteIndex;
+            #endregion
+
+
+            #region Methods
+            public int[] GetRequiredKeys(GameProgressCollection keyList) =>
+                GetKeysIndices(keyList, m_requiredKeys);
+
+            public int[] GetUnlockedKeys(GameProgressCollection keyList) =>
+                GetKeysIndices(keyList, m_unlockedKeys);
+            
+            private int[] GetKeysIndices(GameProgressCollection keyList, List<GameModuleConfig> keys)
+            {
+                if (keyList == null)
+                    return new int[0];
+
+                List<int> keyIndices = new List<int>();
+
+                foreach (GameModuleConfig key in keys)
+                {
+                    int keyIndex = keyList.IndexOf(key);
+                    keyIndices.Add(keyIndex);
+                }
+
+                return keyIndices.ToArray();
+            }
             #endregion
         }
         #endregion
@@ -87,6 +121,17 @@ namespace archipelaGO.Game
                 return (string.Empty, null);
         }
 
+        public (string[] requirements, string[] unlockables) GetProgressKeys(int index)
+        {
+            ModuleNode node = GetNode(index);
+
+            if (node == null || m_progressKeys == null)
+                return (null, null);
+
+            return (GetProgressKeyChain(node.GetRequiredKeys(m_progressKeys)),
+                GetProgressKeyChain(node.GetUnlockedKeys(m_progressKeys)));
+        }
+
         private ModuleNode GetNode(int index)
         {
             if (index < 0 || index >= moduleCount)
@@ -101,6 +146,20 @@ namespace archipelaGO.Game
                 return null;
 
             return m_nodeSprites[index];
+        }
+
+        private string[] GetProgressKeyChain(int[] keys)
+        {
+            if (keys == null || keys.Length <= 0 ||
+                m_progressKeys == null || m_progressKeys.keyCount <= 0)
+                return null;
+
+            string[] keyNames = new string[keys.Length];
+
+            for (int i = 0; i < keyNames.Length; i++)
+                keyNames[i] = m_progressKeys.GetKey(i);
+
+            return keyNames;
         }
         #endregion
     }
