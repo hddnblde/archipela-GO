52ae50d17c4a6f35e4bf13eb1f4ba02151774e36
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-30 12:49:40 +0800

    GameModuleConfig: added timeLimit property.

--- Assets/archipelaGO/Scripts/Game/GameModuleConfig.cs
+++ Assets/archipelaGO/Scripts/Game/GameModuleConfig.cs
@@ -5,6 +5,9 @@ namespace archipelaGO.Game
     public abstract class GameModuleConfig : ScriptableObject
     {
         [SerializeField]
+        private float m_timeLimit = Mathf.Infinity;
+
+        [SerializeField]
         private string m_hint = string.Empty;
 
         [SerializeField]
@@ -16,6 +19,7 @@ namespace archipelaGO.Game
         [SerializeField]
         private bool m_requireTapToContinue = false;
 
+        public float timeLimit => m_timeLimit;
         public string hint => m_hint;
         public string endMessage => m_endMessage;
         public string failMessage => m_failMessage;
