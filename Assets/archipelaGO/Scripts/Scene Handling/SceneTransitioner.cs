577d1b1648e2ead8116aeac0e48eb7b361d1183b
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-30 11:20:46 +0800

    Implemented SceneTransitioner -- which waits for a list of transitionable screens before moving to the next scene.

---
+++ Assets/archipelaGO/Scripts/Scene Handling/SceneTransitioner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace archipelaGO.SceneHandling
+{
+    public class SceneTransitioner : MonoBehaviour
+    {
+        [SerializeField]
+        private List<TransitionableScreen> m_screens = new List<TransitionableScreen>();
+
+        [SerializeField]
+        private Scene m_nextSceneToLoad = Scene.SignIn;
+
+        private void Start() =>
+            StartCoroutine(TransitionRoutine());
+
+        private IEnumerator TransitionRoutine()
+        {
+            foreach (TransitionableScreen screen in m_screens)
+            {
+                if (screen != null)
+                    yield return screen.WaitUntilDone();
+            }
+
+            SceneLoader.LoadScene(m_nextSceneToLoad);
+        }
+    }
+}
