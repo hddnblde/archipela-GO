24d8ec1be561ccd27d4c42be8c436d276ca41ea8
John Carlo Gamboa <hddnblde@gmail.com>
2021-10-30 12:56:31 +0800

    UIWindow: implemented show/hide events.

--- Assets/archipelaGO/Scripts/UI/UIWindow.cs
+++ Assets/archipelaGO/Scripts/UI/UIWindow.cs
@@ -6,6 +6,10 @@ namespace archipelaGO.UI
     public abstract class UIWindow : MonoBehaviour
     {
         private CanvasGroup m_canvasGroup = null;
+        public delegate void Activity();
+
+        public event Activity OnShow;
+        public event Activity OnHide;
 
         public bool shown => m_canvasGroup.blocksRaycasts;
 
@@ -14,12 +18,17 @@ namespace archipelaGO.UI
 
         private void ShowCanvas(bool shown)
         {
-            if (m_canvasGroup == null)
+            if (m_canvasGroup == null || m_canvasGroup.interactable == shown)
                 return;
 
             m_canvasGroup.alpha = (shown ? 1f : 0f);
             m_canvasGroup.blocksRaycasts = shown;
             m_canvasGroup.interactable = shown;
+
+            if (shown)
+                OnShow?.Invoke();
+            else
+                OnHide?.Invoke();
         }
 
         protected virtual void Show() => ShowCanvas(true);
